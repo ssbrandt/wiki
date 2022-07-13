af7a9ea69ee70eeca56a0c47c6a7a8f62b7f7874
ssbrandt <sierrasbrandt@gmail.com>
2022-07-13 11:53:52 -0400

    add /wiki to entry path

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:entry>", views.view_entry, name="entry"),
+    path("wiki/<str:entry>", views.view_entry, name="entry"),
     path("results/", views.search_results, name="results"),
     path("random/", views.random_entry, name="random"),
     path("new/", views.new_entry, name="new"),
